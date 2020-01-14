3cdae52000b2121be682bbd1b9e6d2517095792d
khatba <khatba.husain@gmail.com>
2020-01-14 15:46:14 +0100

    16

---
+++ exo_16.rb
@@ -0,0 +1,23 @@
+puts "Donnez moi votre age ? "
+
+age = gets.chomp
+age1 = age.to_i
+
+i = 0
+while age1 >= 0
+    if i == 0 || i == 1
+        puts "Il y a #{age1} ans tu avais #{i} an"
+
+    elsif age1 == 0 || age1 == 1
+        puts "Il y a #{age1} an tu avais #{i} ans"
+       
+    else
+        puts "Il y a #{age1} ans tu avais #{i} ans"
+    end
+    age1 = age1 - 1
+    i = i + 1
+end 
+    
+
+
+
