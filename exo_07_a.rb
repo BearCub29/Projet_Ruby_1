a91f04171187fc052c782c724f7793c2ee615a07
BearCub29 <poulichet.florian@gmail.com>
2020-01-14 12:27:52 +0100

    7 one

---
+++ exo_07_a.rb
@@ -0,0 +1,9 @@
+puts "Bonjour, c'est quoi ton blase ?"
+user_name = gets.chomp
+puts user_name
+
+#Afficher "Bonjour, c'est quoi ton blase ?"
+#gets.chomp permet de saisir des données provenant de l'utilisateur
+#Assigner la valeur de gets.chomp dans la variable user_name
+#Afficher la valeur de la variable user_name
+
---
+++ exo_07_b.rb
@@ -0,0 +1,10 @@
+puts "Bonjour, c'est quoi ton blase ?"
+print "> "
+user_name = gets.chomp
+puts user_name
+
+#Afficher "Bonjour, c'est quoi ton blase ?"
+#Afficher le signe ">" sur la même ligne que la saisie de données gets.chomp
+#gets.chomp permet de saisir des données provenant de l'utilisateur
+#Assigner la valeur de gets.chomp dans la variable user_name
+#Afficher la valeur de la variable user_name
