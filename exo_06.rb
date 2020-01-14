8a3b99b1fc7cd8263f8855c98e333b883a4949d8
khatba <khatba.husain@gmail.com>
2020-01-14 12:11:32 +0100

    sixth one

---
+++ exo_06.rb
@@ -0,0 +1,11 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+#changer la valeur de chaque variables 
+# ex: number_of_hours_worked_per_day = 10
+
+#afficher "Travail : " + le resultat de l'operation
+#soit number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP = 10 * 5 * 11 = 550
