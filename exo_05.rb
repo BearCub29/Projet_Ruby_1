2ae787d27113175ba1862a7f641fca21ebd7501b
BearCub29 <poulichet.florian@gmail.com>
2020-01-14 12:03:57 +0100

    fifth one

---
+++ exo_05.rb
@@ -0,0 +1,47 @@
+
+# Afficher "On va compter le nombre d'heures de travail à THP" sur terminal.
+puts "On va compter le nombre d'heures de travail à THP"
+
+#Afficher Travail : et le resultat de l'operation entres {} soit 550
+#Afficher En minutes ça fait : et le resultat de l'operation entres {} soit 33,000
+
+puts "Travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Afficher "Et en secondes ?"
+
+puts "Et en secondes ?"
+
+#Afficher le resultat de l'opération soit 1980000
+
+puts 10 * 5 * 11 * 60 * 60
+
+#Afficher "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+# Booleen affiche false
+
+puts 3 + 2 < 5 - 7
+
+#Afficher "Ça fait combien 3 + 2 ?" plus résultat de l'opération entre {} soit 5
+#Afficher "Ça fait combien 5 - 7 ?" plus résultat de l'opération entre {} soit -2
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#Afficher "Ok, c'est faux alors !"
+
+puts "Ok, c'est faux alors !"
+
+#Afficher "C'est drôle ça, faisons-en plus :"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+#Afficher "Est-ce que 5 est plus grand que -2 ?" plus le resulat de l'équation booleenne soit true
+#Afficher "Est-ce que 5 est supérieur ou égal à -2 ? " plus le resulat de l'équation booleenne soit true
+#Afficher "Est-ce que 5 est inférieur ou égal à -2 ?" plus le resulat de l'équation booleenne soit false
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
