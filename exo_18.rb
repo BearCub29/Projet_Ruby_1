4062a0afa8c0ced2de962bbf29b3fd622d556b45
khatba <khatba.husain@gmail.com>
2020-01-14 16:28:52 +0100

    18

---
+++ exo_18.rb
@@ -0,0 +1,17 @@
+tableau = []
+i = 1
+
+while i <= 50
+    if i < 10
+        tableau << "jean.dupont.0#{i}@email.fr"
+    else
+        tableau << "jean.dupont.#{i}@email.fr"
+    end
+    i = i + 1
+
+end
+
+tableau.each do |tab|
+    puts tab
+end
+
